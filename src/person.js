e06e5d47cbd89911d3fd2ac52ad50dd7b9a5b307
olofvi <30499007+olofvi@users.noreply.github.com>
2017-08-29 11:46:12 +0200

    edited the BMI Imperial found a typo

--- src/person.js
+++ src/person.js
@@ -7,3 +7,8 @@ Person.prototype.calculate_bmi = function () {
   calculator = new BMIcalculator();
   calculator.metric_bmi(this);
 };
+
+Person.prototype.calculate_imperial_bmi = function () {
+  calculator = new BMIImperialCalculator();
+  calculator.imperial_bmi(this);
+};
